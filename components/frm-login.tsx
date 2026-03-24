7133983f98aaeffc83464f55698d608e3bed782f
Leonardo da Silva Villa <leosivi@gmail.com>
2026-03-23 20:24:05 -0400

    atualização 23-03-26

--- components/frm-login.tsx
+++ components/frm-login.tsx
@@ -6,15 +6,52 @@ import {
     StyleSheet,
 } from 'react-native';
 
+import { useState }  from "react"; 
+
+import Toast from 'react-native-toast-message';
+
 export default function Login(){
+
+    const [usuario, setUsuario] = useState("");
+    const [senha, setSenha] = useState("");
+
+    const validaLogin = () =>{
+      if(usuario == "admin" && senha == "admin"){
+        alert("Login efetuado com sucesso")
+        Toast.show({
+            type: 'success',
+            text1: 'Sucesso!',
+            text2: 'Login efetuado com sucesso!'
+        })
+      }else{
+        alert("Usuáro ou senha inválidos")
+        Toast.show({
+            type: 'error',
+            text1: 'Erro!',
+            text2: 'Usuário ou senha inválidos'
+        })
+        //npm install react-native-toast-messsage
+      } 
+    }
+
     return(
         <View style={styles.container}>
             <Text style={styles.title}>Área Restrita</Text>
-            <TextInput style={styles.input}/>
-            <TextInput style={styles.input} />
-            <TouchableOpacity style={styles.button}>
+            <TextInput 
+                style={styles.input}
+                value={usuario}
+                onChangeText={setUsuario}
+            />
+            <TextInput 
+                style={styles.input} 
+                value={senha}
+                onChangeText={setSenha}
+                secureTextEntry
+            />
+            <TouchableOpacity style={styles.button} onPress={validaLogin}>
                 <Text style={styles.title}>Login</Text>
             </TouchableOpacity>
+            <Toast />
         </View>
     );
 }
