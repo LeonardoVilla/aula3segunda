60f39400c3d83dff089f7a30d6f7ae2ca6991277
Leonardo da Silva Villa <leosivi@gmail.com>
2026-04-13 19:26:09 -0400

    Atualização do repositorio 13-04

--- components/frm-login.tsx
+++ components/frm-login.tsx
@@ -1,89 +1,58 @@
-import { 
-    View, 
-    Text, 
+import {
+    View,
+    Text,
     TextInput,
     TouchableOpacity,
     StyleSheet,
 } from 'react-native';
 
-import { useState }  from "react"; 
+import { useState } from "react";
 
 import Toast from 'react-native-toast-message';
 
-import { Alert } from 'react-native'
 import { supabase } from '@/lib/supabase'
 
-export default function Login(){
+export default function Login() {
 
     const [usuario, setUsuario] = useState("");
     const [senha, setSenha] = useState("");
     const [loading, setLoading] = useState(false)
 
-    // const validaLogin = () =>{
-    //   if(usuario == "admin" && senha == "admin"){
-    //     alert("Login efetuado com sucesso")
-    //     Toast.show({
-    //         type: 'success',
-    //         text1: 'Sucesso!',
-    //         text2: 'Login efetuado com sucesso!'
-    //     })
-    //   }else{
-    //     alert("Usuáro ou senha inválidos")
-    //     Toast.show({
-    //         type: 'error',
-    //         text1: 'Erro!',
-    //         text2: 'Usuário ou senha inválidos'
-    //     })
-    //     //npm install react-native-toast-messsage
-    //   } 
-    // }
-
     async function validaLogin() {
-    setLoading(true)
-    const { error } = await supabase.auth.signInWithPassword({
-      email: usuario,
-      password: senha,
-    })
-    if (error){
-        Toast.show({
-            type: 'error',
-            text1: 'Erro!',
-            text2: 'Usuário ou senha inválidos'
-        })
-        setLoading(false)
-    }else{
-        Toast.show({
-            type: 'success',
-            text1: 'Sucesso!',
-            text2: 'Login efetuado com sucesso!'
-        })
-    }
-    async function signUpWithEmail() {
         setLoading(true)
-        const {
-        data: { session },
-        error,
-        } = await supabase.auth.signUp({
-        email: usuario,
-        password: senha,
+        const { error } = await supabase.auth.signInWithPassword({
+            email: usuario,
+            password: senha,
         })
-        if (error) Alert.alert(error.message)
-        if (!session) Alert.alert('Please check your inbox for email verification!')
-        setLoading(false)
+        if (error) {
+            Toast.show({
+                type: 'error',
+                text1: 'Erro!',
+                text2: 'Usuário ou senha inválidos'
+            })
+            setLoading(false)
+        } else {
+            Toast.show({
+                type: 'success',
+                text1: 'Sucesso!',
+                text2: 'Login efetuado com sucesso!'
+            })
+            setLoading(false)
+        }
     }
 
-    return(
+    return (
         <View style={styles.container}>
             <Text style={styles.aula}>Aula Segunda</Text>
             <Text style={styles.aula}>Temos que Finalizar</Text>
             <Text style={styles.title}>Área Restrita</Text>
-            <TextInput 
+            <TextInput
                 style={styles.input}
                 value={usuario}
                 onChangeText={setUsuario}
             />
-            <TextInput 
-                style={styles.input} 
+            <TextInput
+                style={styles.input}
                 value={senha}
                 onChangeText={setSenha}
                 secureTextEntry
@@ -97,44 +66,43 @@ export default function Login(){
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
         backgroundColor: 'pink',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    title:{
-        fontFamily:'arial',
+    title: {
+        fontFamily: 'arial',
         fontSize: 24,
         marginBottom: 20,
-        color:'#ffffff'
+        color: '#ffffff'
     },
-    aula:{
-        fontFamily:'arial',
+    aula: {
+        fontFamily: 'arial',
         fontSize: 34,
         marginBottom: 20,
-        color:'#ffffff',
+        color: '#ffffff',
         fontWeight: 'bold',
     },
-    input:{
+    input: {
         width: '90%',
         height: 40,
         backgroundColor: '#ffffff',
-        padding:10,
-        marginBottom:24,
+        padding: 10,
+        marginBottom: 24,
     },
-    button:{
+    button: {
         width: '90%',
         height: 40,
         backgroundColor: 'green',
-        marginBottom:24,
+        marginBottom: 24,
         alignItems: 'center',
         justifyContent: 'center',
         paddingTop: 20,
-    
+
     },
-    buttonDisabled:{
+    buttonDisabled: {
         backgroundColor: 'gray',
     }
-})
-
+})  
